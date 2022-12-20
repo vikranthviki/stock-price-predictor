8ba0b3b4d1b18657ea9310e5ef73a63cf44b4865
vikranthviki <120712108+vikranthviki@users.noreply.github.com>
2022-12-20 23:04:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def add_bg_from_local(image_file):
 
 def main():
     st.set_page_config(page_title='Estimator using SARIMAX ',page_icon="🕗", layout="centered", initial_sidebar_state="auto", menu_items=None)
-    im ="Stock_bb.jpg"
+    im ="Stock_bb.png"
     add_bg_from_local(im)    
     st.title("Asset Price Estimator using time series ")
     Header= '<p style="font-family:Georgia; color:maroon; font-size: 25px;">Enter the company or asset ticker to the get stock prediction for specified days. You can also get the trade strtegy using technical analysis tools 💹</p>'
