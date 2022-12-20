4392241e093cf575fe999d5af1ba534e9b45184a
vikranthviki <120712108+vikranthviki@users.noreply.github.com>
2022-12-20 22:48:14 +0530

    Update stock_market.py

--- stock_market.py
+++ stock_market.py
@@ -300,34 +300,6 @@ def VWAP(hi,lo,cl,v,Trend):
 # In[15]:
 
 
-def optimize_SARIMA(parameters_list, d, D, s, exog):
-   """
-       Return dataframe with parameters, corresponding AIC and SSE
-       
-       parameters_list - list with (p, q, P, Q) tuples
-       d - integration order
-       D - seasonal integration order
-       s - length of season
-       exog - the exogenous variable
-   """
-   
-   results = []
-   
-   for param in tqdm_notebook(parameters_list):
-       try: 
-           model = SARIMAX(exog, order=(param[0], d, param[1]), seasonal_order=(param[2], D, param[3], s)).fit()
-       except:
-           continue
-           
-       aic = model.aic
-       results.append([param, aic])
-       
-   result_df = pd.DataFrame(results)
-   result_df.columns = ['(p,q)x(P,Q)', 'AIC']
-   #Sort in ascending order, lower AIC is better
-   result_df = result_df.sort_values(by='AIC', ascending=True).reset_index(drop=True)
-   
-   return result_df
 
 
 # In[44]:
